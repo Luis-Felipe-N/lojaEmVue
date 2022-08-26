ae2fbdc0285e60426b0fa7b3a1354d081f7d271a
luis.nunes <luis.nunes@mail.uft.edu.br>
2022-08-25 22:33:21 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1>Loja em VUE</h1>
+        <nav>
+           <ul>
+             <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Canecas</a>
+            </li>
+            <li>
+                <a href="">Sapatos</a>
+            </li>
+            <li>
+                <a href="">Sobre</a>
+            </li>
+           </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+    header {
+        display: flex;
+        height: 6rem;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 5%;
+    }
+
+    ul {
+        display: flex;
+        gap: 1rem;
+        list-style: none;
+    }
+
+    li a {
+        color: #fff;
+    }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue'
+
+const produtos = ref([
+  {
+    nome: 'Caneca massa',
+    preco: 19,
+    imagem: 'mug01.png'
+  },
+    {
+    nome: 'Caneca muito massa',
+    preco: 19,
+    imagem: 'mug02.png'
+  },
+    {
+    nome: 'Caneca muito muito massa',
+    preco: 19,
+    imagem: 'mug03.png'
+  }
+])
+
+function handleInput(e) {
+  text.value = e.target.value
+}
+</script>
+
+<template>
+<div class="container">
+  <div v-for="produto in produtos">
+    <div>
+      <img :src="require(`@/assets/${produto.imagem}.png`)" alt="">
+      <strong>{{ produto.nome }}</strong>
+      <span>R$ {{ produto.preco }}</span>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  padding: 0 5%;
+}
+
+.container div {
+  display: flex;
+  flex-direction: column;
+}
+
+.container div strong {
+  font-size: 1.2rem;
+}
+
+</style>
